4fb7718f7e7e0f04b807ffa4164dacbc8efd1cf0
Matthew Barker <matthew.j.barker1@gmail.com>
2021-06-22 20:56:42 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e6c8f53e411318eab89ba716fb55ac9c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '998418ab-928f-46ce-b03f-0fa27629e498'
+};
